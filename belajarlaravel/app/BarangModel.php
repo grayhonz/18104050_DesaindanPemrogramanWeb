365c74f89f4e75ffeca09bd0d79c208023819ae0
grayhonz <zulfiqar.ridho@gmail.com>
2020-12-29 15:41:54 +0700

    Belajar API
    Laravel API

---
+++ belajarlaravel/app/BarangModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BarangModel extends Model
+{
+    //
+    protected $table = 'stuff';
+    protected $primarykey = 'kode_barang';
+}
